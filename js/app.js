d9f31ea8a6156da1a92062f617e44f9fc28ab5b8
Nasir Ahmed <nasirahmed595@gmail.com>
2022-02-28 09:59:39 +0600

    input value clear, previous div clear & add spinner

--- js/app.js
+++ js/app.js
@@ -1,18 +1,36 @@
 const allPlayers = () => {
+    // clear previous div 
+    document.getElementById('player-container').textContent = '';
+    // add spinner
+    document.getElementById('spinner').style.display = "block"
+
+    // main part of the function
     const searchValue = document.getElementById('search-box').value;
     // console.log(searchValue);
+
 
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
     fetch(url)
         .then(res => res.json())
         .then(data => showPlayerDetails(data.player))
+    document.getElementById('spinner').style.display = 'block'
 };
 
 const showPlayerDetails = (players) => {
+    // clear Input field
+    document.getElementById('search-box').value = '';
 
+    // stop spinner
+    document.getElementById('spinner').style.display = 'none'
+    /* if(players){
+data load na hole spinner cholte thakbe
+    } */
+
+    // main part of function
     const threePlayers = players.slice(0, 3);
     // console.log(threePlayers);
     const parent = document.getElementById('player-container')
+
     for (const player of threePlayers) {
         // console.log(player);
         const div = document.createElement('div');
@@ -41,12 +59,26 @@ const details = (pId) => {
 }
 
 const setDetails = (info) => {
-    // console.log(info)
+    console.log(info)
+    if (info.strGender == "Male") {
+        document.getElementById('male').style.display = "block"
+        document.getElementById('female').style.display = "none"
+
+    } else {
+        document.getElementById('female').style.display = "block"
+        document.getElementById('male').style.display = "none"
+
+    }
+
     const containerDiv = document.getElementById('details-container');
+    containerDiv.textContent = '';
     const div = document.createElement('div');
     div.innerHTML = `
     <img src="" alt="">
-    <h1>${info.strPlayer}</h1>
+    <h3>Name: ${info.strPlayer}</h3>
+    <h5>Date Of Birth: ${info.dateBorn}</h5 >
+        <p>Weight: ${info.strWeight}</p>
+
     `
     containerDiv.appendChild(div);
 }
